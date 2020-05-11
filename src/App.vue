2be6648dc472581b5de396f0e450e57c11b4ce59
new-baidu <1627930319@qq.com>
2020-05-11 23:09:00 +0800

    baidu-admin

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <h1>为啥空白的</h1>
+    <div class="box">1</div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'App'
+}
+</script>
+<style lang="less">
+</style>
